3c987e0187496dc84b2e1409d5afd1913ba5f52c
Mamta Kumari <mamta.kumari@credencys.com>
2024-04-01 17:48:18 +0530

    all components add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import Banner from './components/Banner';
+import News from './components/News';
+import BBCNews from './components/BBCNews';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar/>
+        {/* <Banner /> */}
+
+        <Routes>
+        <Route path="/" element={<News key="general" category="general" />}></Route>
+        <Route path="/business" element={<News key="business" category="business" />}></Route>
+        <Route path="/entertainment" element={<News key="entertainment"category="entertainment" />}></Route> 
+        <Route path="/health" element={<News key="health"category="health" />}></Route>
+        <Route path="/science" element={<News key="science"category="science" />}></Route>
+        <Route path="/sports" element={<News key="sports"category="sports" />}></Route>
+        <Route path="/technology" element={<News key="technology"category="technology"/>}></Route>
+        
+        </Routes>
+        <BBCNews/>
+      </div>
+    )
+  }
 }
-
-export default App;
